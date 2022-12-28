a940d0ab7735e990acd900a32dd525ccaba3498f
WhXcjm <whxcjm@gmail.com>
2022-12-28 12:03:37 +0800

    12.28

---
+++ Project.1-抽奖/app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Flask
+
+from flask import Flask,render_template
+from random import randint
+app = Flask(__name__)
+
+lis=['卡牌大师','德邦总管','无畏战车','诡术妖姬','猩红收割者','远古恐惧','正义天使','无极剑圣','牛头酋长','符文法师','亡灵战神','战争女神','众星之子']
+
+@app.route('/index')
+def index():
+    return render_template('index.html',hero=lis)
+
+@app.route('/choujiang')
+def choujiang():
+	
+app.run(debug=True)
