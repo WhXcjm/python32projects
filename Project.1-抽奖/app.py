66cc904f805aaed45eb97fce781f768ba6c6966e
WhXcjm <whxcjm@gmail.com>
2022-12-28 12:52:45 +0800

    project1

--- Project.1-抽奖/app.py
+++ Project.1-抽奖/app.py
@@ -2,7 +2,7 @@
 # Flask
 
 from flask import Flask,render_template
-from random import randint
+import random
 app = Flask(__name__)
 
 lis=['卡牌大师','德邦总管','无畏战车','诡术妖姬','猩红收割者','远古恐惧','正义天使','无极剑圣','牛头酋长','符文法师','亡灵战神','战争女神','众星之子']
@@ -13,5 +13,5 @@ def index():
 
 @app.route('/choujiang')
 def choujiang():
-	
+	return render_template('index.html',hero=lis,h=random.choice(lis))
 app.run(debug=True)
