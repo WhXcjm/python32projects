426fc9955e1297c5b3e7bb6c641366dd9aa7385e
WhXcjm <whxcjm@gmail.com>
2023-01-02 21:57:05 +0800

    repair raw list

--- Project.5-Excel办公/Pre-爬取数据/export.py
+++ Project.5-Excel办公/Pre-爬取数据/export.py
@@ -1,1 +1,21 @@
 # pip install pandas
+import pandas as pd
+import json
+pd.set_option("display.max_rows",5)
+pd.set_option('display.max_columns',None)
+
+with open("list.json","r",encoding='utf8') as f:
+	raw1=json.load(f)
+raw2=[]
+headers=['name','star','actor','type']
+for item in raw1:
+	# try:
+	tmp=[item[key] for key in headers]
+	# print(tmp)
+	# except KeyboardInterrupt:
+	# 	exit(0)
+	# except Exception as e:
+	# 	print(f"err: {e}")
+	raw2.append(tmp)
+data=pd.DataFrame(raw2,columns=headers)
+print(data)
